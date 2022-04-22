b152be17eee6033e9413f358b3a8d94166c01af1
Anthony <tonysim01@hotmail.com>
2022-04-22 13:23:22 +0200

    addtransition

--- app.js
+++ app.js
@@ -1,29 +1,11 @@
-let settings = document.getElementById("settings");
-let gear = document.getElementById("gear");
-let exit = document.getElementById("close");
-let sound = document.querySelector("#sound");
-let time = document.querySelector("#time");
 
 let toggle = document.querySelector(".toggle");
 let body = document.querySelector('body');
-
-
 
 toggle.addEventListener('click',() => {
     body.classList.toggle('open');
   
 });
-
-// exit.addEventListener('click',() => {
-//     settings.style.width="80px";
-//     settings.style.height="80px";
-//     settings.style.borderRadius="80px";
-//     settings.style.top="-15px";
-//     settings.style.left="-15px";
-//     exit.style.display="none";
-//     gear.style.display="flex";
-   
-//  });
 
 let play = document.getElementById("play");
 let pause = document.getElementById("pause");
