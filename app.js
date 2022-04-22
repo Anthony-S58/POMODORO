9f52cdc45a2f8a4b7a192e78ce0bd0c6957d720b
Anthony <tonysim01@hotmail.com>
2022-04-22 20:08:50 +0200

    addConfirmBtn

--- app.js
+++ app.js
@@ -10,12 +10,37 @@ toggle.addEventListener('click',() => {
 let play = document.getElementById("play");
 let pause = document.getElementById("pause");
 
-play.addEventListener('click', () => {
+
+
+play.addEventListener('click', function (){
     play.style.display="none";
     pause.style.display="flex";
+
+ 
 });
+
+min = document.getElementById("#worktimemin".value);
+minutes = document.getElementById("minutes");
+var cpt = min;
+
+// minutes.innerHTML= min;
+
+console.log(min);
+
+
+
+timer = setInterval(function(){
+    if(cpt>0){
+        --cpt;
+        document.getElementById("#secondes").innerHTML = cpt ;
+    }else{
+        clearInterval(timer);
+    }
+}, 1000);
 
 pause.addEventListener('click', () => {
     pause.style.display="none";
     play.style.display="flex";
 });
+
+
